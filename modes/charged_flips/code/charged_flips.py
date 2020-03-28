095bd0b0cb5937d8c4fa3dba9af731e12cb63791
Sean Irby <sean.t.irby@gmail.com>
2020-03-28 09:55:08 -0700

    Charged Flips and Foo mode working

---
+++ modes/charged_flips/code/charged_flips.py
@@ -0,0 +1,107 @@
+from mpf.core.mode import Mode
+
+# - test that charging timer works
+
+class ChargedFlips(Mode):
+    # We dont want players charging if they only have a bit of energy
+    min_energy_value_to_begin_charging = 3
+    flipper_down_delay = 200
+    
+    def mode_init(self):
+        # probably a much better way to define instance variables
+        self.is_charging = None
+        self._current_multiplier = 1
+        self.charge_starting_energy = 0
+        
+
+    @property
+    def current_multiplier(self):
+        """Return current_multiplier."""
+        return self._current_multiplier
+
+
+    def mode_start(self, **kwargs):
+        self.charge_starting_energy = 0
+        self.add_mode_event_handler("s_charge_right_active", self.charge_pressed, side="right")
+        self.add_mode_event_handler("s_charge_left_active", self.charge_pressed, side="left")
+        self.add_mode_event_handler('player_charged_flips_charging_timer_tick', self.charging_tick)
+
+        self.add_mode_event_handler('s_right_flipper_active', self.flipper_pressed, side="right")
+        self.add_mode_event_handler('s_left_flipper_active', self.flipper_pressed, side="left")
+        self.add_mode_event_handler('timer_charge_release_timer_complete', self.reset_multiplier)
+
+    def reset_multiplier(self, **kwargs):
+        self._current_multiplier = 1
+
+    # If my respective flipper is pressed while I'm charging 
+    def flipper_pressed(self, **kwargs):
+        side = kwargs['side']
+        if self.is_charging == side:
+            self.is_charging = None
+            self.machine.events.post('charging_stopped')
+
+            charging_timer = self.machine.timers['charging_timer']
+            charging_timer.stop()
+            charging_timer.reset()
+
+            # only fire off the charge release if I have a charged up multiplier
+            if self._current_multiplier > 1:
+                self.machine.timers['charge_release_timer'].restart()
+                ticks = charging_timer.ticks
+                # 
+                if self._current_multiplier == 2:
+                    self.player.energy = max(self.player.energy - 3, 0)
+                elif self._current_multiplier == 3:
+                    self.player.energy = max(self.player.energy - 6, 0)
+                else:
+                    self.player.energy = 0
+            else:
+                self._current_multiplier = 1
+
+    def charge_pressed(self, **kwargs):
+        side = kwargs['side']
+        # here we decide whether to begin charging
+        # we DONT want to charge if any of the following are true
+        # play a short dismissal sound if any of these conditions are met
+
+        # - i dont energy!
+        if self.player.energy < self.min_energy_value_to_begin_charging:
+            return
+        # - another side is already charging
+        elif self.is_charging:
+            # dont play sound
+            return
+        # - my respective flipper is up
+        elif not self.is_respective_flipper_down(side):
+            return
+        # while unlikely there should be no reason the player can charge while waiting for a charged shot to complete
+        elif self.machine.timers['charge_release_timer'].running:
+            return
+        # - maybe if im in some cool down period quickly after a previous charge was completed
+
+        # kickoff charging timer
+        self.machine.events.post('charging_started')
+        self.is_charging = side
+        # # ensure we start timer at 0
+        self.machine.timers['charging_timer'].restart()
+
+    # check that my respective flipper is down and has been down for a bit of time
+    def is_respective_flipper_down(self, side):
+        s_flipper = 's_right_flipper' if side == 'right' else 's_left_flipper'
+        return self.machine.switch_controller.is_state(s_flipper, False, self.flipper_down_delay)
+
+    def charging_tick(self, **kwargs):
+        print("tick")
+        timer = self.machine.timers['charging_timer']
+        
+        if timer.ticks >= 9 and self.player.energy >= 9:
+            print("BAZ")
+            self._current_multiplier = 4
+        elif timer.ticks >= 6 and timer.ticks < 9 and self.player.energy >= 6:
+            print("BAR")
+            self._current_multiplier = 3
+        elif timer.ticks >= 3 and timer.ticks < 6 and self.player.energy >= 3:
+            print("FOO")
+            self._current_multiplier = 2
+
+
---
+++ tests/test_charged_flips.py
@@ -0,0 +1,146 @@
+from mpf.tests.MpfTestCase import MagicMock
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+class TestChargedFlips(MpfMachineTestCase):
+
+    def _start_game(self):
+        self.machine.playfield.add_ball = MagicMock()
+        # self.machine.ball_controller.num_balls_known = 3
+        self.assertModeRunning('attract')
+        self.hit_switch_and_run("s_trough", 30)
+        self.hit_and_release_switch("s_start")
+
+        # All this stuff may be unnecessary but its what I need to do currently when I run mpf both -x 
+        self.release_switch_and_run("s_trough", 30)
+        self.hit_and_release_switch("s_plunger")
+        self.hit_and_release_switch("s_orbit_right")
+        self.assertIsNotNone(self.machine.game)
+
+
+    def _setup_game(self):
+        self._start_game()
+        self.assertModeRunning('charged_flips')
+
+        # erergy should be 0
+        self.assertEqual(self.machine.game.player_list[0].energy, 0)
+        
+
+    def _energy_up(self, amt):
+        for i in range(amt):
+            self.hit_and_release_switch("s_right_inlane")
+
+    def test_not_enough_energy(self):
+        self._setup_game()
+        # cant charge with 0 energy
+        self.hit_and_release_switch("s_charge_right")
+        # check that timer is not running
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+
+    def test_increase_energy(self):
+        self._setup_game()
+        # hitting right inlane increases energy by 1
+        self._energy_up(1)
+        self.assertEqual(self.machine.game.player_list[0].energy, 1)
+        # hitting left inlane increases energy by 1
+        self._energy_up(1)
+        self.assertEqual(self.machine.game.player_list[0].energy, 2)
+
+    def test_premature_charge(self):
+        self._setup_game()
+        # need at least 3 energy before you can charge
+        self._energy_up(3)
+        self.assertEqual(self.machine.game.player_list[0].energy, 3)
+        # check that timer is not running
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+
+        self.hit_and_release_switch("s_charge_right")
+        self.assertTrue(self.machine.timers['charging_timer'].running)
+
+        # flipping immediately will cancel charging and players energy will be unchanged and players energy will NOT be used
+        self.hit_and_release_switch("s_right_flipper")
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+        self.assertFalse(self.machine.timers['charge_release_timer'].running)
+        self.assertEqual(self.machine.game.player_list[0].energy, 3)
+
+    def test_charge_single_level(self):
+        self._setup_game()
+        # need at least 3 energy before you can charge
+        self._energy_up(3)
+        self.assertEqual(self.machine.game.player_list[0].energy, 3)
+        # check that timer is not running
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+
+        # charging for enough time and flipping uses up 3 energy
+        self.hit_and_release_switch("s_charge_right")
+        self.assertTrue(self.machine.timers['charging_timer'].running)
+        self.advance_time_and_run(10)
+        self.hit_and_release_switch("s_right_flipper")
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+        self.assertTrue(self.machine.timers['charge_release_timer'].running)
+        # 2x multiplier while charge_release_timer is running
+        self.assertEqual(self.machine.modes['charged_flips'].current_multiplier, 2)
+        self.assertEqual(self.machine.game.player_list[0].energy, 0)
+
+        # charge release timer is only active for a short time
+        self.advance_time_and_run(2)
+        self.assertFalse(self.machine.timers['charge_release_timer'].running)
+        # multiplier is back down to 1
+        self.assertEqual(self.machine.modes['charged_flips'].current_multiplier, 1)
+
+    def test_charge_two_levels(self):
+        self._setup_game()
+
+        # bring up to 6
+        self._energy_up(6)
+        self.assertEqual(self.machine.game.player_list[0].energy, 6)
+
+        # charge enough to get a 3x multiplier
+        self.hit_and_release_switch("s_charge_right")
+        self.assertTrue(self.machine.timers['charging_timer'].running)
+        self.advance_time_and_run(10)
+        self.hit_and_release_switch("s_right_flipper")
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+        self.assertTrue(self.machine.timers['charge_release_timer'].running)
+        # 3x multiplier while charge_release_timer is running
+        self.assertEqual(self.machine.modes['charged_flips'].current_multiplier, 3)
+        self.assertEqual(self.machine.game.player_list[0].energy, 0)
+
+    def test_charge_three_levels(self):
+        self._setup_game()
+
+        # bring up to 9
+        self._energy_up(9)
+        self.assertEqual(self.machine.game.player_list[0].energy, 9)
+
+        # charge enough to get a 4x multiplier
+        self.hit_and_release_switch("s_charge_right")
+        self.assertTrue(self.machine.timers['charging_timer'].running)
+        self.advance_time_and_run(20)
+        self.hit_and_release_switch("s_right_flipper")
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+        self.assertTrue(self.machine.timers['charge_release_timer'].running)
+
+        # 4x multiplier while charge_release_timer is running
+        self.assertEqual(self.machine.modes['charged_flips'].current_multiplier, 4)
+        self.assertEqual(self.machine.game.player_list[0].energy, 0)
+
+
+    def test_partial_charge(self):
+        # charge up to 4x but only use 2x
+        self._setup_game()
+
+        # bring up to 9
+        self._energy_up(9)
+        self.assertEqual(self.machine.game.player_list[0].energy, 9)
+
+        # charge enough to get a 2x multiplier
+        self.hit_and_release_switch("s_charge_right")
+        self.assertTrue(self.machine.timers['charging_timer'].running)
+        self.advance_time_and_run(3)
+        self.hit_and_release_switch("s_right_flipper")
+        self.assertFalse(self.machine.timers['charging_timer'].running)
+        self.assertTrue(self.machine.timers['charge_release_timer'].running)
+
+        # 4x multiplier while charge_release_timer is running
+        self.assertEqual(self.machine.modes['charged_flips'].current_multiplier, 2)
+        self.assertEqual(self.machine.game.player_list[0].energy, 6)
