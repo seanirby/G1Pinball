a36f4908c7dcc8db82886c787367cb892ddcfd70
Sean Irby <sean.t.irby@gmail.com>
2020-04-29 18:21:25 -0700

    cleanup

--- activated_env.bat
+++ activated_env.bat
@@ -1,19 +1,2 @@
-rem @echo off
-rem pushd "c:\Users\THINKPAD\AppData\Local"
-rem for /f "delims=" %%a in (' dir /ad /b ') do call :size "%%~fa"
-rem sort /r < "%temp%\dirsize.tmp"
-rem del "%temp%\dirsize.tmp"
-rem popd
-rem pause
-rem goto :eof
-
-rem :size
-rem for /f "tokens=3" %%b in ('dir /s "%~1" 2^>nul ^|find " File(s) "') do set "n=%%b"
-rem set dirsize=%n%
-rem REM set dirsize=%dirsize:,=%
-rem set dirsize=                 %dirsize%
-rem set dirsize=%dirsize:~-18%
-rem >>"%temp%\dirsize.tmp" echo %dirsize% "%~1"
-
 start cmd /k ".\venv\mpf-dev\Scripts\activate.bat"
 
--- runxt.bat
+++ runxt.bat
@@ -1,9 +1,1 @@
-rem command when experimenting with python profiling, assumes that you've got args hardcoded into mpf package (checkout branch 'profilingPerformance')
-rem start cmd /k ".\venv\mpf-dev\Scripts\activate.bat && mpf build production_bundle && cd C:\Users\THINKPAD\dev\mpf && python -m cProfile -o profile_output.cprof mpf\__main__.py"
-
-
-rem start cmd /k ".\venv\mpf-dev\Scripts\activate.bat && python -m mpf"
-
-rem start cmd /k ".\venv\mpf-dev\Scripts\activate.bat && mpf both -xtV"
-
 start cmd /k ".\venv\mpf-dev\Scripts\activate.bat && mpf build production_bundle && mpf both -xP"
